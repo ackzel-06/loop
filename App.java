d51333fa6883d2540678d4c6bc40ddebff4efc20
Ackzel John Borero <111472859+ackzel-06@users.noreply.github.com>
2022-10-03 10:36:30 +0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         
+        // for Loop
+        for (int firstnameCount = 5; firstnameCount != 0; firstnameCount--) {
+            System.out.println("Ackzel");
+        }
+        // While Loop
+        int nicknameCount = 5;
+         while (nicknameCount != 0) {
+            System.out.println("zel");
+            nicknameCount--;
+        }
+         //Do-while Loop
+        int surnameCount = 5;
+          do {
+            System.out.println("Borero");
+            surnameCount--;
+        } while (surnameCount != 0);
+    }
+}
